3ecfd3fc7e479b7cf8bd1eb7e93b7140a4149c05
Sazad Ahemad <mail2sazad@gmail.com>
2026-07-06 02:51:26 +0530

    updated db_config files

--- connection/connect.php
+++ connection/connect.php
@@ -1,16 +1,19 @@
 <?php
 
-//main connection file for both admin & front end
-$servername = "localhost"; //server
-$username = "root"; //username
-$password = ""; //password
-$dbname = "online_rest";  //database
+$config = parse_ini_file(__DIR__ . '/db.config');
 
-// Create connection
-$db = mysqli_connect($servername, $username, $password, $dbname); // connecting 
-// Check connection
-if (!$db) {       //checking connection to DB	
-    die("Connection failed: " . mysqli_connect_error());
+if ($config === false) {
+    die("Unable to load db.config");
 }
 
+$db = mysqli_connect(
+    $config['host'],
+    $config['username'],
+    $config['password'],
+    $config['dbname']
+);
+
+if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
