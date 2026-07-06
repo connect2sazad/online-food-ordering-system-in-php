eecd7270f746f9cdda848fe21602909f89347d97
Sazad Ahemad <mail2sazad@gmail.com>
2026-07-06 17:10:49 +0530

    added a public folder for s3

--- admin/add_menu.php
+++ admin/add_menu.php
@@ -49,7 +49,7 @@ if (isset($_POST['submit'])) {
                     // ✅ S3 UPLOAD
                     $result = $s3->putObject([
                         'Bucket'      => $bucket,
-                        'Key'         => 'dishes/' . $fnew,
+                        'Key'         => 'public/dishes/' . $fnew,
                         'SourceFile'  => $temp,
                         'ContentType' => mime_content_type($temp)
                     ]);
@@ -90,12 +90,24 @@ if (isset($_POST['submit'])) {
 
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Add Menu</title>
-
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+    <title>Ela - Bootstrap Admin Dashboard Template</title>
+    <!-- Bootstrap Core CSS -->
     <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
     <link href="css/helper.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:** -->
+    <!--[if lt IE 9]>
+    <script src="https:**oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https:**oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 </head>
 
 <body class="fix-header">
